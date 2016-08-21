e3a257a53a3588c248b891e9756d1b1e8292cba3
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-08-22 00:07:38 +0200

    Add initial tests and some javadoc

--- src/drawing_lines/core.clj
+++ src/drawing_lines/core.clj
@@ -2,9 +2,11 @@
   (:gen-class))
 
 ;; TODO:
-;; - Add javadocs
-;; - Add some assertions
 ;; - Add tests to create the images
+
+(def ^:const max-rgb-value
+  "The max allowed RGB value."
+  255)
 
 (defn create-canvas
   "Creates a canvas with the specified dimensions"
@@ -13,15 +15,16 @@
    :height height
    :pixels {}})
 
-(defmulti convert-canvas-to (fn [& args] (first args)))
+(defmulti convert-canvas-to
+  "Converts a canvas to 'data' suitable for the provided format."
+  (fn [& args] (first args)))
 (defmethod convert-canvas-to :ppm
   [_ {:keys [width height pixels]}]
-  (letfn [(color->str [[r g b]]
-            (format "%d %d %d" r g b))
+  (letfn [(color->str [[r g b]] (format "%d %d %d" r g b))
           (coord->color [xy]
-            (get pixels xy [255 255 255]))
+                        (get pixels xy [255 255 255]))
           (line->str [coords]
-            (clojure.string/join " " (map (comp color->str coord->color) coords)))]
+                     (clojure.string/join " " (map (comp color->str coord->color) coords)))]
     (let [headers     ["P3"
                        (format "%d %d" width height)
                        "255"]
@@ -30,15 +33,23 @@
                                         (map line->str coord-lines))))))
 
 (defn write-as
+  "Write the canvas to file using the specified format."
   [canvas format file-name]
   (let [data (convert-canvas-to format canvas)]
     (spit file-name data)))
 
-(defn draw-pixel [canvas
-                  xy
-                  [red green blue :as color]]
+(defn draw-pixel
+  "Draw a pixel on the canvas."
+  [canvas
+   [x y :as xy]
+   [red green blue :as color]]
+  ;; Precondition check for x/y out-of-bound and color values outside of allowed range.
+  {:pre [(and (pos? x)
+              (pos? y)
+              (< x (:width canvas))
+              (< y (:height canvas)))
+         (every? #(and (pos? %) (<= % max-rgb-value)) color)]}
   (update canvas :pixels #(assoc % xy color)))
-
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/drawing_lines/core_test.clj
+++ test/drawing_lines/core_test.clj
@@ -1,3 +1,13 @@
 (ns drawing-lines.core-test
   (:require [clojure.test :refer :all]
             [drawing-lines.core :refer :all]))
+
+
+
+(deftest checks-boundaries
+  (is (thrown? AssertionError
+               (-> (create-canvas 10 10)
+                   (draw-pixel [10 10] [1 1 1]))))
+  (is (thrown? AssertionError
+               (-> (create-canvas 10 10)
+                   (draw-pixel [5 5] [0 0 300])))))

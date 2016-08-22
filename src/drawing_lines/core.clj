3c414863005e70c73ca769a92174c5be95701e42
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-08-22 22:37:39 +0200

    Implement Bresenham's line algo
    Adapted (well, mostly copied) from
    http://bpattison.blogspot.se/2010/02/p2.html?m=1
    
    Adapted for arity and inlining functions using `letfn`.

--- src/drawing_lines/core.clj
+++ src/drawing_lines/core.clj
@@ -48,19 +48,86 @@
               (pos? y)
               (< x (:width canvas))
               (< y (:height canvas)))
-         (every? #(and (pos? %) (<= % max-rgb-value)) color)]}
+         (every? #(<= 0 % max-rgb-value) color)]}
   (update canvas :pixels #(assoc % xy color)))
 
-(defn line-to-pixels
+(defn- grid-line
   "Calculates the pixels to fill when drawing a line from x1y1 to x2y2.
-  Uses Bresenham's line algorithm (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)."
-  [[x1 y1] [x2 y2]]
-  nil)
+  Uses Bresenham's line algorithm (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm).
+
+  This implementation is adapted from http://bpattison.blogspot.se/2010/02/p2.html?m=1
+  ;  Copyright (c) Brian Pattison. All rights reserved.
+  ;  The use and distribution terms for this software are covered by the
+  ;  Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+  ;  which can be found in the file epl-v10.html at the root of this distribution.
+  ;  By using this software in any fashion, you are agreeing to be bound by
+  ;  the terms of this license.
+  ;  You must not remove this notice, or any other, from this software.
+  "
+  [[x0 y0] [x1 y1]]
+  (letfn [(x-major-line [x x0 y0 adx ady x-dir y-dir]
+            (if (= x0 x)
+              [[x0 y0 0]]
+              (let [r (x-major-line (+ x-dir x) x0 y0 adx ady x-dir y-dir)
+                    l (last r)]
+                (let [[_ y e] l, e (+ e ady)]
+                  (if (>= (* e 2) adx)
+                    (conj r [x (+ y-dir y) (- e adx)])
+                    (conj r [x y e]))))))
+
+          (y-major-line [y x0 y0 adx ady x-dir y-dir]
+                        (if (= y0 y)
+                          [[x0 y0 0]]
+                          (let [r (y-major-line (+ y-dir y) x0 y0 adx ady x-dir y-dir)
+                                l (last r)]
+                            (let [[x _ e] l, e (+ e adx)]
+                              (if (>= (* e 2) ady)
+                                (conj r [(+ x-dir x) y (- e ady)])
+                                (conj r [x y e]))))))]
+    (let [dx  (- x1 x0)
+          dy  (- y1 y0)
+          m   (if (= dx 0)  ; check for divid by zero
+                (* dy java.lang.Double/POSITIVE_INFINITY)
+                (/ dy dx))
+          adx (Math/abs dx)
+          ady (Math/abs dy)
+          am  (if (neg? m) (* -1 m) m)]
+      (cond
+                                        ; q0
+        (and (< x0 x1) (<= 0 m 1))
+        (x-major-line x1 x0 y0 adx ady -1 +1)
+                                        ; q1
+        (and (< x0 x1) (and (> 0 m) (>= m -1)))
+        (x-major-line x1 x0 y0 adx ady -1 -1)
+                                        ; q4
+        (and (< x1 x0) (<= 0 m 1))
+        (x-major-line x1 x0 y0 adx ady +1 -1)
+                                        ; q5
+        (and (< x1 x0) (>= 0 m -1))
+        (x-major-line x1 x0 y0 adx ady +1 +1)
+                                        ; q2
+        (and (< y1 y0) (> -1 m))
+        (y-major-line y1 x0 y0 adx ady +1 +1)
+                                        ; q3
+        (and (< y1 y0) (< 1 m))
+        (y-major-line y1 x0 y0 adx ady -1 +1)
+                                        ; q6
+        (and (< y0 y1) (> -1 m))
+        (y-major-line y1 x0 y0 adx ady -1 -1)
+                                        ; q7
+        (and (< y0 y1) (< 1 m))
+        (y-major-line y1 x0 y0 adx ady +1 -1)
+                                        ; no slope, not a line just a point
+                                        ; happens when x0 == x1 and y0 == y1
+        true []))))
+
+(defn line-to-pixels [x1y1 x2y2]
+  (map (fn [[x y _]] [x y]) (grid-line x1y1 x2y2)))
 
 (defn draw-line
   "Draws a line from x1y1 to x2y2 using the specified color"
   [canvas [x1 y1 :as c1] [x2 y2 :as c2] rgb]
-  (reduce #(draw-pixel canvas % rgb) canvas (line-to-pixels c1 c2)))
+  (reduce #(draw-pixel %1 %2 rgb) canvas (line-to-pixels c1 c2)))
 
 (defn -main
   "I don't do a whole lot ... yet."
